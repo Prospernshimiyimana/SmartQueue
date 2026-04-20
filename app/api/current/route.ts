610c10678de8e3c450333c9c79679f93593e4ebe
Prosper Nshimiyimana <nshimiyimanaprosper97.com>
2026-04-20 11:00:32 +0200

    fix api current build collect error

--- app/api/current/route.ts
+++ app/api/current/route.ts
@@ -1,12 +1,11 @@
 import { prisma } from "../../lib/prisma";
-import { NextResponse } from "next/server";
 
 export const dynamic = "force-dynamic";
 
 export async function GET() {
   const counter = await prisma.counter.findFirst();
 
-  return NextResponse.json({
+  return Response.json({
     current: counter?.current || 1,
   });
 }
