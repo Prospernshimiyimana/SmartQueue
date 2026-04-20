097fe12226ad8a8ae9697a28f41a3a60fd832e5c
Prosper Nshimiyimana <nshimiyimanaprosper97.com>
2026-04-20 10:53:13 +0200

    fix api current build issue

--- app/api/current/route.ts
+++ app/api/current/route.ts
@@ -1,9 +1,12 @@
 import { prisma } from "../../lib/prisma";
+import { NextResponse } from "next/server";
+
+export const dynamic = "force-dynamic";
 
 export async function GET() {
   const counter = await prisma.counter.findFirst();
 
-  return Response.json({
+  return NextResponse.json({
     current: counter?.current || 1,
   });
 }
